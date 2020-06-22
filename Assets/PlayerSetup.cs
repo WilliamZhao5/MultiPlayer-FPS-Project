7498864237ee1b9c392d56b530357622a2333bde
yuzhao <yuzhao5@illinois.edu>
2020-06-22 18:00:37 -0500

    Player Network Done
    Finish building Network Manager and PlayerSetup. Now Players  can build the host server and see each others in the game. However, the movemennt information has not been able to be updated through the network.

---
+++ Assets/PlayerSetup.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerSetup : NetworkBehaviour
+{
+    [SerializeField]
+    Behaviour[] componentsToDisable;
+
+    private Camera sceneCamera;
+
+    //Disable the Player Motor and Controller if the Player is not the local player
+    //if the Player is the local player, then remove the Scnene Camera
+    void Start()
+    {
+        if (!isLocalPlayer)
+        {
+            for (int i = 0; i < componentsToDisable.Length; i++)
+            {
+                componentsToDisable[i].enabled = false;
+            }
+        } else
+        {
+            sceneCamera = Camera.main;
+            if (sceneCamera != null)
+            {
+                sceneCamera.gameObject.SetActive(false);
+
+            }
+        }
+    }
+
+    void OnDisable()
+    {
+        if (sceneCamera != null)
+        {
+            sceneCamera.gameObject.SetActive(true);
+
+        }
+    }
+}
